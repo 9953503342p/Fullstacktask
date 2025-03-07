e7eb5505eb9cafcebf432bf6dd08aa8ffbfa1987
9953503342p <168403027+9953503342p@users.noreply.github.com>
2025-03-07 09:01:47 +0530

    task

---
+++ backendsimple - Copy/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 9080;
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+
+app.get('/api/greet', (req, res) => {
+  const name = req.query.name;
+  if (!name) {
+    return res.status(400).json({ error: 'Name is required.' });
+  }
+  res.json({ message: `Hello, ${name}! Welcome to Younglabs.` });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
